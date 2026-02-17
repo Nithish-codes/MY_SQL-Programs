30fbd5c7c882045f58eb4189470183693cd27705
Nithish Kumar P <nithish2790@gmail.com>
2026-02-17 22:00:32 +0530

    Updated Experiment 1

--- 1.sql
+++ 1.sql
@@ -1,28 +1,30 @@
-create DATABASE if not exists db1;
-use db1;
-
+CREATE DATABASE IF NOT EXISTS db1;
+USE db1;
 
 -- Creating the Students table
-drop table if exists Student;
-CREATE TABLE Student(
-    StudentID INT PRIMARY KEY,
-    Name VARCHAR(50) NOT NULL,
-    Age INT CHECK (Age >= 18),
+DROP TABLE IF EXISTS Student;
+
+CREATE TABLE Student (
+    StudentID  INT PRIMARY KEY,
+    Name       VARCHAR(50) NOT NULL,
+    Age        INT CHECK (Age >= 18),
     Department VARCHAR(10)
 );
 
 -- Inestring values
-insert into Student value(1,"Ananya Sharma",20,"CSE");
-insert into Student value(2, "Ravi Kumar", 21, "ECE");
-insert into Student value(3, "Meera Iyer", 19, "IT");
-insert into Student value(4, "Nithish Kumar P", 19, "CSE");
+INSERT INTO Student VALUES (1, "Ananya Sharma",   20, "CSE");
+INSERT INTO Student VALUES (2, "Ravi Kumar",      21, "ECE");
+INSERT INTO Student VALUES (3, "Meera Iyer",      19, "IT");
+INSERT INTO Student VALUES (4, "Nithish Kumar P", 19, "CSE");
 
 -- Updating values in record
-update Student set Age = 25 where StudentId = 3;
+UPDATE Student 
+SET    Age = 25 
+WHERE  StudentID = 3;
 
 -- Deleting a record
-delete  from Student where StudentID = 3;
+DELETE FROM Student 
+WHERE  StudentID = 3;
 
 -- Querying table
-select * FROM Student;
-
+SELECT * FROM Student;
