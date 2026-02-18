efb8018339bfc163f97ce6ad4a2a088a7279f394
Nithish Kumar P <nithish2790@gmail.com>
2026-02-18 08:55:07 +0530

    Updated Second Experiment

--- 2.sql
+++ 2.sql
@@ -1,40 +1,39 @@
-create database if not exists db2;
-use db2;
+CREATE DATABASE IF NOT EXISTS db2;
+USE db2;
 
-set FOREIGN_KEY_CHECKS = 0;
-drop table if exists Department;
-drop table if exists Student;
-set FOREIGN_KEY_CHECKS = 1;
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE IF EXISTS Department;
+DROP TABLE IF EXISTS Student;
+SET FOREIGN_KEY_CHECKS = 1;
+
 -- Creating Department table
-
-create table Department(
-	DepartmentID int primary key,
-    DepartmentName varchar(50) not null
+CREATE TABLE Department (
+    DepartmentID INT PRIMARY KEY,
+    DepartmentName VARCHAR(50) NOT NULL
 );
 
 -- Creating Student table with Foreign Key
-
-create table Student(
-	StudentID int primary key,
-    StudentName varchar(50) not null,
-    DeptID int,
-    foreign key (DeptID) references Department(DepartmentID)
+CREATE TABLE Student (
+    StudentID INT PRIMARY KEY,
+    StudentName VARCHAR(50) NOT NULL,
+    DeptID INT,
+    FOREIGN KEY (DeptID) REFERENCES Department(DepartmentID)
 );
 
 -- Inserting into Department
-insert into Department values (1, "Computer Science");
-insert into Department values (2, "Electronics");
-insert into Department values (3, "Mechanical");
-insert into Department values (4, "Computer Science");
+INSERT INTO Department VALUES (1, "Computer Science");
+INSERT INTO Department VALUES (2, "Electronics");
+INSERT INTO Department VALUES (3, "Mechanical");
+INSERT INTO Department VALUES (4, "Computer Science");
 
 -- Inserting valid data into Student
-insert into Student values(101, "Arjun Reddy", 1);
-insert into Student values(102, "Lakshmi Menon", 2);
-insert into Student values(103, "Rahul Singh", 1);
-insert into Student values(104, "Nithish Kumar P", 1);
+INSERT INTO Student VALUES (101, "Arjun Reddy", 1);
+INSERT INTO Student VALUES (102, "Lakshmi Menon", 2);
+INSERT INTO Student VALUES (103, "Rahul Singh", 1);
+INSERT INTO Student VALUES (104, "Nithish Kumar P", 1);
 
 -- Trying to insert with invalid foreign key INSERT INTO
--- insert into Student values (105, "Invalid Entry", 5);
+-- INSERT INTO Student VALUES (105, "Invalid Entry", 5);
 
-select * from Department;
-select * from Student;
+SELECT * FROM Department;
+SELECT * FROM Student;
